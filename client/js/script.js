f069508ae2c4d0c9d402c0e3aa207ca097615104
jeroenslemmer <jslemmer@davinci.nl>
2023-09-26 08:16:34 +0200

    added sections and display of films

--- client/js/script.js
+++ client/js/script.js
@@ -1,23 +1,63 @@
-function getAndDisplayFilms() {
+
+function displayFilms(films){
+     // Sorteer de films op basis van hun titel
+    films.sort((a, b) => b.title < a.title);
+    // Maak een lijstitem voor elke film en voeg deze toe aan de lijst       
+    let container = document.querySelector('#filmsList tbody');
+    container.innerHTML = '';
+    for (const film of films) {
+        const row = document.createElement('tr');
+        row.innerHTML = '<td>'+ film.title + '</td>';
+        container.appendChild(row);
+    }
+}
+
+function displayFilmsRanked(films){
+    // Sorteer de films op basis van hun ranking
+   films.sort((a, b) => b.rank - a.rank);
+   // Maak een lijstitem voor elke film en voeg deze toe aan de lijst       
+   let container = document.getElementById('rankingList');
+   container.innerHTML = '';
+   for (const film of films) {
+       const listItem = document.createElement('li');
+       listItem.textContent = `${film.title} - ${film.rank}`;
+       container.appendChild(listItem);
+   }
+}
+
+function getAndDisplayFilms(displayFilms) {
     // Haal de lijst met films op van de backend API
     fetch('http://localhost:5000/films')
         .then(response => response.json())
         .then(films => {
-            console.log(films)
-            // Sorteer de films op basis van hun ranking
-            films.sort((a, b) => b.rank - a.rank);
-            console.log(films);
-
-            // Maak een lijstitem voor elke film en voeg deze toe aan de lijst
-            for (const film of films) {
-                const listItem = document.createElement('li');
-                console.dir(film);
-                listItem.textContent = `${film.title} - ${film.rank}`;
-                filmList.appendChild(listItem);
-            }
+            displayFilms(films);
         });
 }
 
-// Roep de functie aan om de lijst met films op te halen en weer te geven
-getAndDisplayFilms();
+function activateSection(section){  
+    let sections = document.querySelectorAll('section');
+    sections.forEach((section)=>{section.classList.remove('active')});
+    section.classList.add('active');
+}
 
+function clickedHome(event){
+    if (sectionHome.classList.contains('active')) return;
+    activateSection(sectionHome);
+    getAndDisplayFilms(displayFilmsRanked);
+}
+
+function clickedFilms(event){
+    if (sectionFilms.classList.contains('active')) return;
+    activateSection(sectionFilms);
+    getAndDisplayFilms(displayFilms);
+}
+
+
+btnHome.onclick = clickedHome;
+btnFilms.onclick = clickedFilms;
+// btnUsers.onclick = clicked;
+// btnAccount.onclick = clicked;
+
+// Roep de functie aan om de lijst met films op te halen en weer te geven
+
+clickedHome();
